758fb275dd630012313940ac38262cad0ec9e9fc
elliot <elliot@ivy.rip>
2025-02-09 23:06:51 -0600

    colorscheme added blink support

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -8,7 +8,7 @@ return {
                 default = { "lazydev", "snippets", "lsp", "path", "buffer" },
                 providers = { lazydev = { name = "LazyDev", module = "lazydev.integrations.blink", score_offset = 100 } },
             },
-            appearance = { use_nvim_cmp_as_default = true, nerd_font_variant = "mono" },
+            appearance = { nerd_font_variant = "mono" },
             signature = { enabled = true }, -- show signature help
             completion = {
                 -- show lsp docs of option
