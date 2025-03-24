967450787b88d596e739ef3be7234a07c3afd536
elliot <elliot@ivy.rip>
2025-03-24 18:21:37 -0500

    add yank highlight

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,10 @@
+-- autocommands
+-- auto loaded in config/lazy.lua
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking text",
+    group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
