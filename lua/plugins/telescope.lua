a2dc40f0091b0e5e41910022ce03ef18d3348c0a
elliot <elliot@ivy.rip>
2025-04-17 11:52:54 -0500

    build telescope fzf native with make instead of cmake

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
         "nvim-telescope/telescope.nvim",
         branch = "0.1.x",
         dependencies = {
-            { "nvim-telescope/telescope-fzf-native.nvim", build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release" },
+            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
         },
         opts = {},
         config = function()
