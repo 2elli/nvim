5135b9f0b0d8a3907babd22970a7d89312a1c495
elliot <elliot@ivy.rip>
2025-04-22 16:03:40 -0500

    move snacks to dependency plugins folder
    to be loaded first...

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -71,39 +71,4 @@ return {
             { "<leader>gr", "<CMD>Gitsigns reset_hunk<CR>", desc = "Gitsigns: reset hunk" },
         },
     },
-    -- misc
-    {
-        "folke/snacks.nvim",
-        priority = 1000, -- load first
-        lazy = false,
-        opts = {
-            -- indent lines
-            indent = { animate = { enabled = false } },
-            -- disable some features for big files 1.5MB i think
-            bigfile = { enabled = true },
-            -- undo tree
-            picker = { enabled = true },
-        },
-        config = function(_, opts)
-            -- setup
-            require("snacks").setup(opts)
-
-            -- make debug helpers
-            _G.dd = function(...) Snacks.debug.inspect(...) end
-            _G.bt = function() Snacks.debug.backtrace() end
-
-            -- lsp file rename for oil
-            vim.api.nvim_create_autocmd("User", {
-                pattern = "OilActionsPost",
-                callback = function(event)
-                    if event.data.actions.type == "move" then
-                        Snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
-                    end
-                end,
-            })
-        end,
-        keys = {
-            { "<leader>u", function() Snacks.picker.undo() end, desc = "Undo Tree" },
-        },
-    },
 }
