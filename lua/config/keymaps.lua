822392745ad25afa2c59004180c14816dfd85ddb
elliot <elliot@ivy.rip>
2025-09-25 11:45:25 -0500

    format and change move line maps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,32 +1,28 @@
 -- keymaps
 -- auto loaded in config/lazy.lua
 
+local map = vim.keymap.set
+
 -- no op leader key
-vim.keymap.set("n", "<SPACE>", "<Nop>", { desc = "nop leader key" })
+map("n", "<SPACE>", "<Nop>", { desc = "nop leader key" })
 
 -- keep page dwn + up centered
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "center page up" })
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "center page down" })
+map("n", "<C-u>", "<C-u>zz", { desc = "center page up" })
+map("n", "<C-d>", "<C-d>zz", { desc = "center page down" })
 
 -- keep searching dwn + up centered
-vim.keymap.set("n", "n", "nzzzv", { desc = "center searching" })
-vim.keymap.set("n", "N", "Nzzzv", { desc = "center searching" })
+map("n", "n", "nzzzv", { desc = "center searching" })
+map("n", "N", "Nzzzv", { desc = "center searching" })
 
 -- clear search highlight
-vim.keymap.set("n", "<Esc>", "<CMD>nohlsearch<CR>", { desc = "clear search highlights" })
+map("n", "<Esc>", "<CMD>nohlsearch<CR>", { desc = "clear search highlights" })
 
--- move lines
-vim.keymap.set("v", "<M-k>", ":m '<-2<CR>gv=gv", { desc = "move selection up" })
-vim.keymap.set("v", "<M-j>", ":m '>+1<CR>gv=gv", { desc = "move selection down" })
-vim.keymap.set("n", "<M-k>", ":m .-2<CR>==", { desc = "move line up" })
-vim.keymap.set("n", "<M-j>", ":m .+1<CR>==", { desc = "move line down" })
+-- move lines (from lazy)
+map("n", "<M-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "move line down" })
+map("n", "<M-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "move line up" })
+map("v", "<M-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "move line down" })
+map("v", "<M-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "move line up" })
 
 -- append lines
-vim.keymap.set("n", "J", "mzJ`z", { desc = "append next line to current" })
-vim.keymap.set("n", "<M-J>", "mzj:m .-2<CR>==J`z", { desc = "append current line to next" })
-
--- delete without copying to register
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "delete without copying to register" })
-
--- lsp
-vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, {desc = "Lsp: Hover"})
+map("n", "J", "mzJ`z", { desc = "append next line to current" })
+map("n", "<M-J>", "mzj:m .-2<CR>==J`z", { desc = "append current line to next" })
