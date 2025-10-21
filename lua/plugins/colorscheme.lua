0ef20ca2ac4519c8f3146ba7df5ae93a2fd0cecb
elliot <elliot@ivy.rip>
2025-10-21 17:41:36 -0500

    use transparent bg colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,6 +4,9 @@ return {
         lazy = false,
         priority = 1000,
         config = function()
+            require("nightfox").setup({
+                options = { transparent = true },
+            })
             vim.cmd.colorscheme("carbonfox")
         end,
     },
