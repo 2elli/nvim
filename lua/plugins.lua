c8efcdc6addf16ad2c64bb8d7253e1773a15a843
elliot <elliot@ivy.rip>
2025-12-07 15:34:05 -0600

    add conform formatter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,6 +5,7 @@ vim.pack.add({
     { src = "https://github.com/nvim-treesitter/nvim-treesitter", version="master" },
     { src = "https://github.com/neovim/nvim-lspconfig" },
     { src = "https://github.com/mason-org/mason.nvim" },
+    { src = "https://github.com/stevearc/conform.nvim" },
 })
 
 -- color
@@ -40,4 +41,11 @@ require("nvim-treesitter.configs").setup({
 
 -- lsp
 require("mason").setup()
-vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format() end, { desc = "lsp format" })
+-- formatter
+require("conform").setup({
+    default_format_opts = { lsp_format = "fallback", async = true },
+    formatters_by_ft = {
+        ocaml = {"ocamlformat"}
+    }
+})
+vim.keymap.set("n", "<leader>lf", function() require("conform").format() end, { desc = "lsp format" })
