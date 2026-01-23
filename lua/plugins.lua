361fbd48433b086375c58cafe58e449ffd5fd770
elliot <elliot@ivy.rip>
2026-01-23 13:17:13 -0600

    create keymap shortcut

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -38,30 +38,30 @@ require("nvim-treesitter.configs").setup({
 
 -- quick files
 require("harpoon"):setup()
-vim.keymap.set("n", "<leader>a", function() require("harpoon"):list():add() end, { desc = "Harpoon: add file to list" })
-vim.keymap.set("n", "<leader>f", function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end,
+map("n", "<leader>a", function() require("harpoon"):list():add() end, { desc = "Harpoon: add file to list" })
+map("n", "<leader>f", function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end,
     { desc = "Harpoon: toggle quick menu" })
-vim.keymap.set("n", "<C-n>", function() require("harpoon"):list():next() end, { desc = "Harpoon: goto next mark" })
-vim.keymap.set("n", "<C-p>", function() require("harpoon"):list():prev() end, { desc = "Harpoon: goto previous mark" })
+map("n", "<C-n>", function() require("harpoon"):list():next() end, { desc = "Harpoon: goto next mark" })
+map("n", "<C-p>", function() require("harpoon"):list():prev() end, { desc = "Harpoon: goto previous mark" })
 
 -- explorer
 require("oil").setup()
-vim.keymap.set("n", "<leader>e", function() require("oil").toggle_float() end, { desc = "Oil: toggle float" })
+map("n", "<leader>e", function() require("oil").toggle_float() end, { desc = "Oil: toggle float" })
 
 -- git
 require("gitsigns").setup()
-vim.keymap.set("n", "<leader>gg", "<CMD>Gitsigns<CR>", { desc = "Gitsigns: menu" })
-vim.keymap.set("n", "<leader>gb", "<CMD>Gitsigns blame_line<CR>", { desc = "Gitsigns: blame line" })
-vim.keymap.set("n", "<leader>gB", "<CMD>Gitsigns blame<CR>", { desc = "Gitsigns: blame menu" })
-vim.keymap.set("n", "<leader>gd", "<CMD>vert rightb Gitsigns diffthis<CR>", { desc = "Gitsigns: open git diff" })
-vim.keymap.set("n", "<leader>gn", "<CMD>Gitsigns next_hunk<CR>", { desc = "Gitsigns: goto next hunk" })
-vim.keymap.set("n", "<leader>gp", "<CMD>Gitsigns prev_hunk<CR>", { desc = "Gitsigns: goto prev hunk" })
-vim.keymap.set("n", "<leader>gr", "<CMD>Gitsigns reset_hunk<CR>", { desc = "Gitsigns: reset hunk" })
+map("n", "<leader>gg", "<CMD>Gitsigns<CR>", { desc = "Gitsigns: menu" })
+map("n", "<leader>gb", "<CMD>Gitsigns blame_line<CR>", { desc = "Gitsigns: blame line" })
+map("n", "<leader>gB", "<CMD>Gitsigns blame<CR>", { desc = "Gitsigns: blame menu" })
+map("n", "<leader>gd", "<CMD>vert rightb Gitsigns diffthis<CR>", { desc = "Gitsigns: open git diff" })
+map("n", "<leader>gn", "<CMD>Gitsigns next_hunk<CR>", { desc = "Gitsigns: goto next hunk" })
+map("n", "<leader>gp", "<CMD>Gitsigns prev_hunk<CR>", { desc = "Gitsigns: goto prev hunk" })
+map("n", "<leader>gr", "<CMD>Gitsigns reset_hunk<CR>", { desc = "Gitsigns: reset hunk" })
 
 -- fzf
-vim.keymap.set("n", "<leader>/", function() require("fzf-lua").live_grep_native() end, { desc = "fzf grep" })
-vim.keymap.set("n", "<leader>F", function() require("fzf-lua").files() end, { desc = "fzf grep" })
-vim.keymap.set("n", "<leader>k", function() require("fzf-lua").keymaps() end, { desc = "fzf grep" })
+map("n", "<leader>/", function() require("fzf-lua").live_grep_native() end, { desc = "fzf grep" })
+map("n", "<leader>F", function() require("fzf-lua").files() end, { desc = "fzf grep" })
+map("n", "<leader>k", function() require("fzf-lua").keymaps() end, { desc = "fzf grep" })
 
 -- lsp servers
 require("mason").setup()
@@ -73,7 +73,7 @@ require("conform").setup({
         rust = { "rustfmt" },
     },
 })
-vim.keymap.set({ "n", "x" }, "<leader>lf", function() require("conform").format() end, { desc = "conform format" })
+map({ "n", "x" }, "<leader>lf", function() require("conform").format() end, { desc = "conform format" })
 
 -- autocomplete
 require("blink.cmp").setup({
