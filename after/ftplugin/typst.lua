f57190b83adea619613d6fabe5ca323c36d2646a
elliot <elliot@ivy.rip>
2026-03-01 01:56:56 -0600

    move some ft specific config to ftplugin

---
+++ after/ftplugin/typst.lua
@@ -0,0 +1,1 @@
+vim.cmd.packadd("typst-preview.nvim")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -110,22 +110,3 @@ require("blink.cmp").setup({
         ["<C-d>"] = { "scroll_documentation_down", "fallback" },
     },
 })
-
--- writing
-vim.api.nvim_create_user_command("TypstPreview",
-    function()
-        require("typst-preview").setup({})
-        vim.cmd.TypstPreview()
-    end,
-    { desc = "run typst preview" }
-)
-
--- rocq
--- dont use default maps
-vim.g.coqtail_nomap = 1
-map("n", "<leader>cc", "<cmd>RocqStart<cr>", { desc = "Rocq: Launch coqtail in current buf" })
-map("n", "<leader>cq", "<cmd>RocqStop<cr>", { desc = "Rocq: Stop coqtail in current buf" })
-map("n", "<leader>cj", "<cmd>RocqNext<cr>", { desc = "Rocq: Send next sentence" })
-map("n", "<leader>ck", "<cmd>RocqUndo<cr>", { desc = "Rocq: Step back 1 sentence" })
-map("n", "<leader>cl", "<cmd>RocqToLine<cr>", { desc = "Rocq: Send all sentences up to line" })
-map("n", "<leader>cL", "<cmd>RocqToLine<cr>", { desc = "Rocq: Send all sentences" })
