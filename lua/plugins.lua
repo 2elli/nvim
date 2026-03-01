26d4baad7d579b755b5b975b959c43601f75cc37
elliot <elliot@ivy.rip>
2026-03-01 01:54:41 -0600

    remove mason.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,7 +11,6 @@ vim.pack.add({
     { src = "https://github.com/lewis6991/gitsigns.nvim" },
     -- lsp
     { src = "https://github.com/neovim/nvim-lspconfig" },
-    { src = "https://github.com/mason-org/mason.nvim" },
     { src = "https://github.com/stevearc/conform.nvim" },
     { src = "https://github.com/mfussenegger/nvim-lint" },
     { src = "https://github.com/saghen/blink.cmp", version = vim.version.range("1.*") },
