ad05b78f3dbe8d4dd6ef3f2042e6eeb91bbbcab8
elliot <elliot@ivy.rip>
2026-03-01 01:47:05 -0600

    init flake.nix for deps

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+    description = "Neovim Packages";
+
+    inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    outputs = { self, nixpkgs, ... }:  let
+        pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    in {
+        packages."x86_64-linux" = {
+            default = pkgs.buildEnv {
+                name = "nvim-packages";
+                paths = with pkgs; [
+                    ### lsp ###
+                    basedpyright
+                    ruff
+                    lua-language-server
+                    typescript-language-server
+                    tinymist
+                    zls
+                    haskellPackages.haskell-language-server
+                    nil
+
+                    ### lint ###
+                    typos
+                    haskellPackages.hlint
+
+                    ### formatter ###
+                    haskellPackages.fourmolu
+                    nixfmt
+                ];
+            };
+        };
+    };
+}
