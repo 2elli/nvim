e29223e73441f87c36816a5f0fb977dc28b43130
elliot <elliot@ivy.rip>
2026-03-03 14:21:15 -0600

    format and style nix flake

--- flake.nix
+++ flake.nix
@@ -4,12 +4,13 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
   outputs =
-    { self, nixpkgs, ... }:
+    { nixpkgs, ... }:
     let
-      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
     in
     {
-      packages."x86_64-linux" = {
+      packages.${system} = {
         default = pkgs.buildEnv {
           name = "nvim-packages";
           paths = with pkgs; [
