4328d500688ecc69e5e938a34a937ef02fc5bda4
elliot <elliot@ivy.rip>
2026-07-26 13:23:48 -0500

    add pack clean custom command

--- lua/cmds.lua
+++ lua/cmds.lua
@@ -19,3 +19,10 @@ vim.api.nvim_create_user_command(
 )
 
 vim.api.nvim_create_user_command("PackUpdate", function() vim.pack.update() end, {})
+
+vim.api.nvim_create_user_command("PackCleanNotActive", function()
+    vim.iter(vim.pack.get())
+        :filter(function(x) return not x.active end)
+        :map(function(x) return x.spec.name end)
+        :totable()
+end, {})
